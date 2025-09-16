dc18ddc13e0ba7dbaedd507b8a0523ea0f59fa9f
Huỳnh Như <147977974+nhu2004@users.noreply.github.com>
2025-09-16 21:33:18 +0700

    Initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,258 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './g_gi_n_vui_v_-_4.png'
+import menu_2 from './g_gi_n_vui_v_-_8_1_1.png'
+import menu_3 from './burger_-_7.png'
+import menu_4 from './m_jolly_-_6_7-compressed_1.jpg'
+import menu_5 from './m_n_tr_ng_mi_ng_-_3.png'
+import menu_6 from './th_c_u_ng_-_5_6_1.png'
+
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './1PCS.jpg'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './EGGTART-4.jpg'
+import food_18 from './EGGTART-1.jpg'
+import food_19 from './4-Chewy-Cheese.jpg'
+import food_20 from './2-taro.jpg'
+import food_21 from './AQUAFINA.jpg'
+import food_22 from './7UP_CAN.jpg'
+import food_23 from './LIPTON-M.jpg'
+import food_24 from './PEPSI-ZERO-M.jpg'
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import remove_icon_cross from './remove_icon_cross.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    remove_icon_cross,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Combo",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Gà",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Hambuger",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Mỳ ý",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Tráng miệng",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Nước uống",
+        menu_image: menu_6
+    }
+
+]
+export const food_list = [
+    {
+        _id: "1",
+        name: "Greek salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Combo"
+    },
+    {
+        _id: "2",
+        name: "Veg salad",
+        image: food_2,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Combo"
+    }, {
+        _id: "3",
+        name: "Clover Salad",
+        image: food_3,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Combo"
+    }, {
+        _id: "4",
+        name: "Gà Xốt Mắm Tỏi",
+        image: food_5,
+        price: 24000,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Combo"
+    }, {
+        _id: "5",
+        name: "Gà Xốt Mắm Tỏi",
+        image: food_4,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Gà"
+    }, {
+        _id: "6",
+        name: "Peri Peri Rolls",
+        image: food_6,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Gà"
+    }, {
+        _id: "7",
+        name: "Chicken Rolls",
+        image: food_7,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Gà"
+    }, {
+        _id: "8",
+        name: "Veg Rolls",
+        image: food_8,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Hambuger"
+    }, {
+        _id: "9",
+        name: "Ripple Ice Cream",
+        image: food_9,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Hambuger"
+    }, {
+        _id: "10",
+        name: "Fruit Ice Cream",
+        image: food_10,
+        price: 22,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Hambuger"
+    }, {
+        _id: "11",
+        name: "Jar Ice Cream",
+        image: food_11,
+        price: 10,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Mỳ ý"
+    }, {
+        _id: "12",
+        name: "Vanilla Ice Cream",
+        image: food_12,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Mỳ ý"
+    },
+    {
+        _id: "13",
+        name: "Chicken Sandwich",
+        image: food_13,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Mỳ ý"
+    },
+     {
+        _id: "14",
+        name: "Cup Cake",
+        image: food_17,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Tráng miệng"
+    }, {
+        _id: "15",
+        name: "Vegan Cake",
+        image: food_18,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Tráng miệng"
+    }, {
+        _id: "16",
+        name: "Butterscotch Cake",
+        image: food_19,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Tráng miệng"
+    }, {
+        _id: "17",
+        name: "Sliced Cake",
+        image: food_20,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Tráng miệng"
+    }, {
+        _id: "18",
+        name: "Aquafina 500ml ",
+        image: food_21,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Nước uống"
+    }, {
+        _id: "19",
+        name: "7UP Lon",
+        image: food_22,
+        price: 22,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Nước uống"
+    }, {
+        _id: "20",
+        name: "Lipton",
+        image: food_23,
+        price: 10,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Nước uống"
+    }, {
+        _id: "21",
+        name: "Pepsi Phúc Bồn Tử Lon",
+        image: food_24,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Nước uống"
+    }
+]
