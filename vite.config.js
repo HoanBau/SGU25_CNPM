53aad65fde08f66e856c7997f2dc342638ddffa7
Huỳnh Như <147977974+nhu2004@users.noreply.github.com>
2025-11-12 13:08:26 +0700

    cập nhật lần 30

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,   // hoặc host: '0.0.0.0'
+    port: 5173,   // giữ port cũ, hoặc đổi nếu muốn
+  }
 })
